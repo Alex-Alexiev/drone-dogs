8155a2adf28046acbdc9ae50b6a2c89e3244a297
Kevin Angers <kevin.angers@mail.utoronto.ca>
2025-04-07 14:17:47 -0400

    Add new onnx file, launch command to run sh

--- run.sh
+++ run.sh
@@ -15,3 +15,9 @@ ros2 launch px4_autonomy_modules mavros.launch.py
 # ros2 topic pub -1 /offsets/z std_msgs/msg/Float64 "{data: 0.1}"
 
 # ros2 bag record -a
+
+# ros2 launch ros_deep_learning video_source.ros2.launch
+
+# ros2 launch ros_deep_learning detectnet.ros2.launch output:=display://0 model_path:=/home/jetson/ros2_ws/src/px4_autonomy_modules/models/ssd-mobilenet-apr7.onnx input_blob:=input_0 output_cvg:=scores output_bbox:=boxes threshold:=0.2 class_labels_path:=/home/jetson/ros2_ws/src/px4_autonomy_modules/models/labels.txt
+
+
